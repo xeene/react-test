c6646aa2b7b683ad81808c54e385d3c39442cc2d
nikita <nik.veras@mail.ru>
2024-05-21 18:00:12 +0900

    Эксперименты с выводом

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,41 @@
-import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import MoviesCard from "./components/moviesCard.jsx";
 import UserList from "./components/userList.jsx";
 import TextDisplay from "./components/Text.jsx";
 
 function App() {
-
   // return <TextDisplay/>
 
   const [users, setUsers] = useState([]);
 
   // Запрос может быть выполнен либо внутри метода жизненного цикла, если компонент является компонентом класса (???), либо внутри хука React useEffect(), если компонент является функциональным компонентом (?????).
 
-
-    useEffect(() => {
-
-      function fetchData() {
-        const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-        fetch(apiUrl)
-        .then(responce => responce.json())
-        .then(data => {
+  useEffect(() => {
+    function fetchData() {
+      const apiUrl = "https://jsonplaceholder.typicode.com/users";
+      fetch(apiUrl)
+        .then((responce) => responce.json())
+        .then((data) => {
           setUsers(data);
-          console.log('Data:', data)});
-      };
+          console.log("Data:", data);
+        });
+    }
 
-      fetchData();
-
-    }, []);
+    fetchData();
+  }, []);
   return (
     <>
       <div className="movies-wrapper">
-       
-          <h1>Movie List</h1>
-          <div className="movie-list">
-            {
-movies &&
-<UserList users={users}></UserList>
-            }
-          
-          </div>
+        <h1>Movie List</h1>
+        <div className="movie-list">
+          {users && <UserList users={users}></UserList>}
+        </div>
       </div>
     </>
-  )
-
-
+  );
 }
 
-export default App
+export default App;
