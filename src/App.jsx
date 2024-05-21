ac7fb8764e74a4c86f617ce6c61aca852319cd42
nikita <nik.veras@mail.ru>
2024-05-21 17:27:23 +0900

    Не работает вывод

--- src/App.jsx
+++ src/App.jsx
@@ -5,68 +5,37 @@ import './App.css'
 import MoviesCard from "./components/moviesCard.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([]);
 
-  const [moviesList, setMoviewsList] = useState('Nikita')
+  // Запрос может быть выполнен либо внутри метода жизненного цикла, если компонент является компонентом класса (???), либо внутри хука React useEffect(), если компонент является функциональным компонентом (?????).
+
 
     useEffect(() => {
 
+      function fetchData() {
+        const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+        fetch(apiUrl)
+        .then(responce => responce.json())
+        .then(data => console.log('Data:', data));
+      };
+
+      fetchData();
 
     }, []);
-
-
-
-  // const moviesList = [
-  //     {
-  //         id: 1,
-  //         name: 'Nikita',
-  //         desc: 'best moview'
-  //     },
-  //     {
-  //         id: 2,
-  //         name: 'Sveta',
-  //         desc: 'not best moview'
-  //     }
-  // ]
-
-    function fetchMovies() {
-        fetch('games.com/api/games', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            apiKey: 'dsadasd2131dasdas'
-        }).then(serverData =>  serverData.json()).then(data => {
-            setMoviewsList(data.list)
-        }).catch()
-    }
   return (
     <>
-      <div>
-
-          <button onClick={fetchMovies}></button>
-      <div className="moviews-wrapper">
-          {moviesList.map(movie => <MoviesCard key={movie.id} movie={movie}/>)}
+      <div className="movies-wrapper">
+          <h1>Movie List</h1>
+          <div className="movie-list">
+            <ul>
+              {
+                movies.map((movie) => (
+                  <li key = {movie.id}>{movie.name}</li>
+                ))
+              }
+            </ul>
+          </div>
       </div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
