9d1d2d85b123fea896c2fb21dce59f75bfafb178
true nikita <nik.veras@mail.ru>
2024-05-23 09:07:51 +0300

    mobx example and refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,23 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import MoviesCard from "./components/moviesCard.jsx";
 import AnimeList from "./components/animeList.jsx";
-import UserList from "./components/userList.jsx";
-import TextDisplay from "./components/Text.jsx";
+import {useEffect} from "react";
+import {animeStore} from "./store/animeStore.js";
+import AnimeActions from "./components/animeActions.jsx";
 
 function App() {
-  // return <TextDisplay/>
-  const [anime, setAnime] = useState([]);
 
- 
   useEffect(() => {
-    function fetchData() {
-      const apiUrl = "https://kitsu.io/api/edge/anime";
-      fetch(apiUrl, {
-        'Accept': "application/vnd.api+json",
-        "Content-Type": "application/vnd.api+json",
-      })
-        .then((responce) => responce.json())
-        .then((data) => {
-          setAnime(data.data);
-          // console.log("Data.Date:", data.data);
-        });
-    }
-
-    fetchData();
+    animeStore.getAnimeFromServer()
   }, []);
+
   return (
-    <>
-      <div className="movies-wrapper">
-        <h1>Movie List</h1>
-        <div className="movie-list">
-          <AnimeList anime={anime}></AnimeList>
-        </div>
+    <div className="movies-wrapper">
+      <h1>Movie List</h1>
+      <div className="movie-list">
+        <AnimeList></AnimeList>
       </div>
-    </>
+      <AnimeActions/>
+    </div>
   );
 }
 
